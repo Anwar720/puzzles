5b27668af1ccf74716c02fdfa51e6f5d084761c7
Anwar <Anwarj223@gmail.com>
2022-01-21 15:46:44 -0500

    updated

--- Caesar_cipher/script.js
+++ Caesar_cipher/script.js
@@ -1,5 +1,5 @@
-// Function takes in plainstext and encrypts it using Caesar cipher
-function CaesarCipher(plainText){
+// Function takes in plainstext and number of shifts then encrypts it using Caesar cipher
+function CaesarCipher(plainText,shift){
     
     // Ciphertext will contain the shifted letters
     let cipherText = '';
@@ -14,18 +14,18 @@ function CaesarCipher(plainText){
         //loops through the letters array to find index of plaintText letter in the letters array
         for(let j = 0; j<letters.length; j++){
             //checks to see if letter is found in the letters array
-            if(plainText[i] === letters[j]){
+            if(plainText[i].toLowerCase() === letters[j]){
 
                 // Adds 3 to the index to shift the letter by 3 and
                 //uses modulus 25 to make sure that all the indexes are with in 25 
                 // hence creating a cycle from a to z and z to a 
-                cipherText += letters[( j + 3) % 25];
+                cipherText += letters[( j + shift) % 25];
             }
         }
     }
     return cipherText;
 }
 
-console.log(CaesarCipher('hello'));
+console.log(CaesarCipher('Hello',3));
 
 
